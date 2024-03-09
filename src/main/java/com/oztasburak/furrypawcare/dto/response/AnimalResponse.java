1894c5843f3e359d12543a31f8a895ba2ba49ba1
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:22:34 +0300

    Update AnimalResponse.java

--- src/main/java/com/oztasburak/furrypawcare/dto/response/AnimalResponse.java
+++ src/main/java/com/oztasburak/furrypawcare/dto/response/AnimalResponse.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,4 +28,6 @@ public class AnimalResponse {
     private LocalDate dateOfBirth;
 
     private OnlyCustomerResponse customer;
+
+    private List<OnlyAppointmentResponse> appointments;
 }
