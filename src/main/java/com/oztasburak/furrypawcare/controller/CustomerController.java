139e126924ec554c50c1cdc07facbe891c837ac5
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 21:28:56 +0300

    Create CustomerController.java

---
+++ src/main/java/com/oztasburak/furrypawcare/controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.oztasburak.furrypawcare.controller;
+
+import com.oztasburak.furrypawcare.dto.request.CustomerRequest;
+import com.oztasburak.furrypawcare.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.oztasburak.furrypawcare.config.BaseURL.BASE_URL;
+
+@RestController
+@RequestMapping(BASE_URL + "customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<> (customerService.getAllResponses (), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") Long id) {
+        return new ResponseEntity<> (customerService.getResponseById (id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody CustomerRequest customerRequest) {
+        return new ResponseEntity<> (customerService.create (customerRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(
+            @PathVariable("id") Long id,
+            @Valid @RequestBody CustomerRequest customerRequest
+    ) {
+        return new ResponseEntity<> (customerService.update (id, customerRequest), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
+        customerService.deleteById (id);
+        return new ResponseEntity<> (HttpStatus.NO_CONTENT);
+    }
+}
