e9ed36069d268db6dcd27728beba1a045cbbe1c1
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:11:04 +0300

    Create AvailableDateService.java

---
+++ src/main/java/com/oztasburak/furrypawcare/service/AvailableDateService.java
@@ -0,0 +1,81 @@
+package com.oztasburak.furrypawcare.service;
+
+import com.oztasburak.furrypawcare.config.BaseService;
+import com.oztasburak.furrypawcare.config.ModelMapperService;
+import com.oztasburak.furrypawcare.dto.request.AvailableDateRequest;
+import com.oztasburak.furrypawcare.dto.response.AvailableDateResponse;
+import com.oztasburak.furrypawcare.entity.AvailableDate;
+import com.oztasburak.furrypawcare.repository.AvailableDateRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AvailableDateService implements BaseService<AvailableDate, AvailableDateRequest, AvailableDateResponse> {
+    private final AvailableDateRepository availableDateRepository;
+    private final ModelMapperService modelMapperService;
+
+    @Override
+    public AvailableDate getById (Long id)
+        {
+            return availableDateRepository.findById(id)
+                    .orElseThrow (() -> new EntityNotFoundException ("AvailableDate with ID" + id + " not found"));
+        }
+
+    @Override
+    public AvailableDateResponse getResponseById (Long id)
+        {
+            return modelMapperService
+                    .forResponse ()
+                    .map (getById (id), AvailableDateResponse.class);
+        }
+
+    @Override
+    public List<AvailableDate> getAll ()
+        {
+            return availableDateRepository.findAll ();
+        }
+
+    @Override
+    public List<AvailableDateResponse> getAllResponses ()
+        {
+            return getAll ()
+                    .stream ().map (availableDate -> modelMapperService
+                            .forResponse ()
+                            .map (availableDate, AvailableDateResponse.class))
+                    .toList ();
+        }
+
+    @Override
+    public AvailableDateResponse create (AvailableDateRequest availableDateRequest)
+        {
+            AvailableDate availableDate = modelMapperService
+                    .forRequest ()
+                    .map (availableDateRequest, AvailableDate.class);
+
+            return modelMapperService
+                    .forResponse ()
+                    .map (availableDateRepository.save (availableDate), AvailableDateResponse.class);
+        }
+
+    @Override
+    public AvailableDateResponse update (Long id , AvailableDateRequest availableDateRequest)
+        {
+            AvailableDate doesAvailableDateExist = getById (id);
+
+            modelMapperService.forRequest ().map (availableDateRequest, doesAvailableDateExist);
+
+            return modelMapperService
+                    .forResponse ()
+                    .map (availableDateRepository.save (doesAvailableDateExist), AvailableDateResponse.class);
+        }
+
+    @Override
+    public void deleteById (Long id)
+        {
+            availableDateRepository.delete (getById (id));
+        }
+}
