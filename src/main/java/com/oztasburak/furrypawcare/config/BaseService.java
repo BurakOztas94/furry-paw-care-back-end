17f1a1e52d9ba0cf291eb99e3c8a126ce82919c6
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 21:11:11 +0300

    Update BaseService.java

--- src/main/java/com/oztasburak/furrypawcare/config/BaseService.java
+++ src/main/java/com/oztasburak/furrypawcare/config/BaseService.java
@@ -1,10 +1,12 @@
 package com.oztasburak.furrypawcare.config;
 
+import java.util.List;
+
 public interface BaseService<Entity, EntityRequest, EntityResponse> {
     Entity getById(Long id);
     EntityResponse getResponseById(Long id);
-    Entity getAll();
-    EntityResponse getAllResponses();
+    List<Entity> getAll();
+    List<EntityResponse> getAllResponses();
     EntityResponse create(EntityRequest entityRequest);
     EntityResponse update(Long id, EntityRequest entityRequest);
     void deleteById(Long id);
