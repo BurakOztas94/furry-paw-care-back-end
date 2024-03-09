154a0e5bd1c9a8fdcd4851d9c95987b3f09134a7
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:31:14 +0300

    Update AnimalRepository.java

--- src/main/java/com/oztasburak/furrypawcare/repository/AnimalRepository.java
+++ src/main/java/com/oztasburak/furrypawcare/repository/AnimalRepository.java
@@ -2,6 +2,8 @@ package com.oztasburak.furrypawcare.repository;
 
 import com.oztasburak.furrypawcare.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
