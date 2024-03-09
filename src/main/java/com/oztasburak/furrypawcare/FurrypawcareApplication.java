2c1f1699ab6871c6c97238a308e1dc0609c39122
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 20:29:54 +0300

    Create Workspace

---
+++ src/main/java/com/oztasburak/furrypawcare/FurrypawcareApplication.java
@@ -0,0 +1,13 @@
+package com.oztasburak.furrypawcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurrypawcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurrypawcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oztasburak/furrypawcare/FurrypawcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oztasburak.furrypawcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurrypawcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
