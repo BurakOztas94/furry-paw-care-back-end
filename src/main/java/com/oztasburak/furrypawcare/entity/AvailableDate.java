994920dd9ac59e12b0bb99d75df412bc44bbc7e0
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:10:56 +0300

    Create AvailableDate.java

---
+++ src/main/java/com/oztasburak/furrypawcare/entity/AvailableDate.java
@@ -0,0 +1,26 @@
+package com.oztasburak.furrypawcare.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AvailableDate {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDate availableDate;
+
+    @ManyToOne
+    @JoinColumn(columnDefinition = "doctor_id", referencedColumnName = "id")
+    private Doctor doctor;
+}
