0cac84531210a5b472724316699439287b7a920a
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:22:45 +0300

    Create Appointment.java

---
+++ src/main/java/com/oztasburak/furrypawcare/entity/Appointment.java
@@ -0,0 +1,30 @@
+package com.oztasburak.furrypawcare.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Appointment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime appointmentDate;
+
+    @ManyToOne
+    @JoinColumn(columnDefinition = "doctor_id", referencedColumnName = "id")
+    private Doctor doctor;
+
+    @ManyToOne
+    @JoinColumn(columnDefinition = "animal_id", referencedColumnName = "id")
+    private Animal animal;
+}
