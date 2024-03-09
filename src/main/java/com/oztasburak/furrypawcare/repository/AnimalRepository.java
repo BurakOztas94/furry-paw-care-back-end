4de5452bbd6da929e67b75ec227a82bd6cb2c6d5
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 23:33:58 +0300

    Update AnimalRepository.java

--- src/main/java/com/oztasburak/furrypawcare/repository/AnimalRepository.java
+++ src/main/java/com/oztasburak/furrypawcare/repository/AnimalRepository.java
@@ -2,8 +2,15 @@ package com.oztasburak.furrypawcare.repository;
 
 import com.oztasburak.furrypawcare.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    List<Animal> findByName(String name);
+
+    @Query(value = "SELECT animal.* FROM animal INNER JOIN customer ON animal.customer_id = customer.id WHERE customer.name = %?1%", nativeQuery = true)
+    List<Animal> findByCustomerName(String customerName);
 }
