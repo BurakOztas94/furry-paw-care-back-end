018ff17af932abd963d3474d34f9965dec6272f2
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:10:51 +0300

    Create OnlyAvailableDateResponse.java

---
+++ src/main/java/com/oztasburak/furrypawcare/dto/response/OnlyAvailableDateResponse.java
@@ -0,0 +1,18 @@
+package com.oztasburak.furrypawcare.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OnlyAvailableDateResponse {
+    private Long id;
+
+    private LocalDate availableDate;
+}
