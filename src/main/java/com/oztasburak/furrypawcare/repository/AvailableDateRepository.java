8a6a1cb588b04fdeb0fafd6ed2ec915f3d825e33
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-09 22:31:18 +0300

    Update AvailableDateRepository.java

--- src/main/java/com/oztasburak/furrypawcare/repository/AvailableDateRepository.java
+++ src/main/java/com/oztasburak/furrypawcare/repository/AvailableDateRepository.java
@@ -2,6 +2,8 @@ package com.oztasburak.furrypawcare.repository;
 
 import com.oztasburak.furrypawcare.entity.AvailableDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AvailableDateRepository extends JpaRepository<AvailableDate, Long> {
 }
