4cda0acb936a010bf2dcbddd7f85eaaee743137f
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-10 14:15:24 +0300

    Update AppointmentRepository.java

--- src/main/java/com/oztasburak/furrypawcare/repository/AppointmentRepository.java
+++ src/main/java/com/oztasburak/furrypawcare/repository/AppointmentRepository.java
@@ -4,6 +4,11 @@ import com.oztasburak.furrypawcare.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    Optional<Appointment> findByDoctorIdAndAppointmentDate(Long doctorId, LocalDateTime appointmentDate);
 }
