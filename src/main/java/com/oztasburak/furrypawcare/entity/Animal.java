128279de99c52223f55c559d7a48e1c4262be664
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-16 21:22:38 +0300

    Update Animal.java

--- src/main/java/com/oztasburak/furrypawcare/entity/Animal.java
+++ src/main/java/com/oztasburak/furrypawcare/entity/Animal.java
@@ -31,7 +31,7 @@ public class Animal {
 
     private LocalDate dateOfBirth;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(columnDefinition = "customer_id", referencedColumnName = "id")
     private Customer customer;
 
