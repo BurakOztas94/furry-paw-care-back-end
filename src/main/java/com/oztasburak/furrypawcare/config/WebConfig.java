2cd083792b31a41f59805b74b57c37e2e58b5fa0
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-19 23:25:19 +0300

    Update WebConfig.java

--- src/main/java/com/oztasburak/furrypawcare/config/WebConfig.java
+++ src/main/java/com/oztasburak/furrypawcare/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://24-week-project-question-app1-5mh4.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
